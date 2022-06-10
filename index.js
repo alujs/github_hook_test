4b79230845782f802fcbb202bb9fb390f706c9df
limz <steinernein@gmail.com>
2022-06-09 21:02:33 -0400

    Logic.

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const { Octokit } = require('octokit')
-const octokit = new Octokit({
-    auth: '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'
-  })
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { processUrl, hasCurrentReview, updateReview, createReview } = require('./helpers');
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 
-app.post('/', bodyParser.json(), (req, res) => {
-    console.log('HELLO req', req.body)
+app.post('/', bodyParser.json(), async (req, res) => {
+  console.log('HEY we got an action,', req.body.action);
+  if (req.body.action === 'opened') {
+    try {
+      const meta = processUrl(req.body);
+
+      meta.errors = await processFilesFromPullRequest(meta);
+  
+      if (await hasCurrentReview ) {
+        await updateReview(meta);
+      } else {
+        await createReview(meta);
+      }
+    } catch(e) {
+      return res.status(500).send(e);
+    }
+  }
+
+  res.status(202).send(true);
 });
 
 
